86175f650071a2d31831e553bae89af9493a2204
sipwarehouse944@gmail.com <sipwarehouse944@gmail.com>
2023-12-09 20:17:39 +0000

    Commit and Sync

--- Sipdatawarehouse/models/obt_title_author.sql
+++ Sipdatawarehouse/models/obt_title_author.sql
@@ -4,7 +4,7 @@ with
     d_date as (select * from {{ ref("dim_date") }}),
     d_publishers as (select * from {{ ref("dim_publishers") }})
 
-select f.*,d_authors.*, d_date.*, d_publishers.* 
+select f.*, d_authors.*, d_date.*, d_publishers.*
 from f_title_author as f
 left join d_authors on f.authorkey = d_authors.authorkey
 left join d_publishers on f.publisherskey = d_publishers.publisherskey
